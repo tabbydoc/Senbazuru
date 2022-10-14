661b7f4082a47f74f9c5f70435ed192828acd9f3
Viacheslav Paramonov <info@paramonov.by>
2022-10-14 18:09:08 +0800

    Add Utils.cs (parsing named parametrs)

---
+++ Senbazuru/ExcelImporter/HirarchicalExtraction/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Senbazuru.HirarchicalExtraction
+{
+    public class Utils
+    {
+        public static Dictionary<string, string> NamedParams(string[] args)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            string[] kv = new string[2];
+
+            foreach (string param in args)
+            {
+                try
+                {
+                    kv = param.Split('=');
+                    result.Add(kv[0], kv[1]);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine($"Ошибка: {e.Message}");
+                }
+            }
+            return result;
+
+
+
+        }
+    }
+}
